01e087f426b15984c23cda3c64ff0c2de1f9d185
dbfv <quanbfclan@gmail.com>
2025-02-28 15:08:01 +0700

    comments added

--- Part1/Validator.cs
+++ Part1/Validator.cs
@@ -3,25 +3,25 @@
 namespace Part1 {
 
     public static class Validator {
-        public static string[] errorList = new string[3];
-        public static void ValidateString(string S, Label label) {
-            if (S.Length == 0) {
-                errorList[0] = "String length must be within 1-40 characters!";
-                errorList[1] = "Only uppercase letters are acceptable!";
+        public static string[] errorList = new string[3]; //create array to store the error messages with 3 messages max
+        public static void ValidateString(string S, Label label) {  //static method to validate String S
+            if (S.Length == 0) { //if the string is empty 
+                errorList[0] = "String length must be within 1-40 characters!"; //reset the messages to default
+                errorList[1] = "Only uppercase letters are acceptable!"; //reset the message to default
             }
-            else {
-                if (S.Length > 0 && S.Length <= 40) {
-                    errorList[0] = "String length is within 1-40 characters.";
+            else { //if not empty 
+                if (S.Length > 0 && S.Length <= 40) { //valid string
+                    errorList[0] = "String length is within 1-40 characters."; //show this message
                 }
-                else {
+                else { // if not empty and out of range, show below message
                     errorList[0] = "String length must be within 1-40 characters!";
                 }
 
-                bool IsCapitalLetter = Regex.IsMatch(S, @"^[A-Z]+$");
-                if (!IsCapitalLetter) {
+                bool IsCapitalLetter = Regex.IsMatch(S, @"^[A-Z]+$");  // create boolean variable to check if all chars are capital
+                if (!IsCapitalLetter) {                             //if not, show below message
                     errorList[1] = "Only uppercase letters are acceptable!";
                 }
-                else {
+                else { //if yes, show below message
                     errorList[1] = "All characters are uppercase letters!";
                 }
             }
