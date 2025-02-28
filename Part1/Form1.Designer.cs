ade3b83b997415b8c4070d0caa1090eade6cec4f
dbfv <quanbfclan@gmail.com>
2025-03-01 02:04:08 +0700

    label and Textbox for sort added

--- Part1/Form1.Designer.cs
+++ Part1/Form1.Designer.cs
@@ -41,16 +41,13 @@ namespace Part1 {
             label2 = new Label();
             label1 = new Label();
             textBox1 = new TextBox();
-            button5 = new Button();
-            label7 = new Label();
-            label8 = new Label();
-            label9 = new Label();
-            label11 = new Label();
             pictureBox1 = new PictureBox();
             label10 = new Label();
             label12 = new Label();
             label13 = new Label();
             numericUpDown1 = new NumericUpDown();
+            label7 = new Label();
+            textBox5 = new TextBox();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
             SuspendLayout();
@@ -60,22 +57,24 @@ namespace Part1 {
             label5.AutoSize = true;
             label5.BackColor = Color.White;
             label5.ForeColor = Color.Black;
-            label5.Location = new Point(12, 136);
+            label5.Location = new Point(10, 121);
             label5.Name = "label5";
-            label5.Size = new Size(101, 18);
+            label5.Size = new Size(89, 16);
             label5.TabIndex = 17;
             label5.Text = "Word count: 0";
             // 
             // button6
             // 
             button6.BackColor = Color.Transparent;
+            button6.Enabled = false;
             button6.ForeColor = Color.Black;
-            button6.Location = new Point(644, 12);
+            button6.Location = new Point(564, 11);
             button6.Name = "button6";
-            button6.Size = new Size(95, 60);
+            button6.Size = new Size(83, 53);
             button6.TabIndex = 22;
             button6.Text = "RUN";
             button6.UseVisualStyleBackColor = false;
+            button6.Click += button6_Click;
             // 
             // label6
             // 
@@ -83,9 +82,9 @@ namespace Part1 {
             label6.BackColor = Color.Transparent;
             label6.Font = new Font("Segoe UI", 14F);
             label6.ForeColor = Color.Cyan;
-            label6.Location = new Point(460, 9);
+            label6.Location = new Point(402, 8);
             label6.Name = "label6";
-            label6.Size = new Size(48, 29);
+            label6.Size = new Size(43, 27);
             label6.TabIndex = 20;
             label6.Text = "N: ";
             label6.Click += label6_Click;
@@ -96,9 +95,9 @@ namespace Part1 {
             label4.BackColor = Color.Transparent;
             label4.Font = new Font("Segoe UI", 14F);
             label4.ForeColor = Color.Cyan;
-            label4.Location = new Point(12, 272);
+            label4.Location = new Point(10, 242);
             label4.Name = "label4";
-            label4.Size = new Size(158, 29);
+            label4.Size = new Size(143, 27);
             label4.TabIndex = 10;
             label4.Text = "Output Code: ";
             // 
@@ -106,9 +105,9 @@ namespace Part1 {
             // 
             button4.BackColor = Color.Transparent;
             button4.ForeColor = Color.Black;
-            button4.Location = new Point(644, 94);
+            button4.Location = new Point(564, 84);
             button4.Name = "button4";
-            button4.Size = new Size(95, 60);
+            button4.Size = new Size(83, 53);
             button4.TabIndex = 15;
             button4.Text = "PRINT";
             button4.UseVisualStyleBackColor = false;
@@ -117,9 +116,9 @@ namespace Part1 {
             // 
             button3.BackColor = Color.Transparent;
             button3.ForeColor = Color.Black;
-            button3.Location = new Point(644, 278);
+            button3.Location = new Point(564, 247);
             button3.Name = "button3";
-            button3.Size = new Size(95, 25);
+            button3.Size = new Size(83, 22);
             button3.TabIndex = 14;
             button3.Text = "Copy!";
             button3.UseVisualStyleBackColor = false;
@@ -128,9 +127,9 @@ namespace Part1 {
             // 
             button2.BackColor = Color.Transparent;
             button2.ForeColor = Color.Black;
-            button2.Location = new Point(644, 222);
+            button2.Location = new Point(564, 197);
             button2.Name = "button2";
-            button2.Size = new Size(95, 25);
+            button2.Size = new Size(83, 22);
             button2.TabIndex = 13;
             button2.Text = "Copy!";
             button2.UseVisualStyleBackColor = false;
@@ -139,9 +138,9 @@ namespace Part1 {
             // 
             button1.BackColor = Color.Transparent;
             button1.ForeColor = Color.Black;
-            button1.Location = new Point(644, 171);
+            button1.Location = new Point(564, 152);
             button1.Name = "button1";
-            button1.Size = new Size(95, 25);
+            button1.Size = new Size(83, 22);
             button1.TabIndex = 12;
             button1.Text = "Copy!";
             button1.UseVisualStyleBackColor = false;
@@ -149,19 +148,23 @@ namespace Part1 {
             // 
             // textBox4
             // 
-            textBox4.Location = new Point(211, 278);
+            textBox4.Enabled = false;
+            textBox4.Location = new Point(185, 247);
             textBox4.Name = "textBox4";
             textBox4.ReadOnly = true;
-            textBox4.Size = new Size(410, 25);
+            textBox4.Size = new Size(359, 23);
             textBox4.TabIndex = 11;
+            textBox4.TextChanged += textBox4_TextChanged;
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(211, 222);
+            textBox3.Enabled = false;
+            textBox3.Location = new Point(185, 197);
             textBox3.Name = "textBox3";
             textBox3.ReadOnly = true;
-            textBox3.Size = new Size(410, 25);
+            textBox3.Size = new Size(359, 23);
             textBox3.TabIndex = 9;
+            textBox3.TextChanged += textBox3_TextChanged;
             // 
             // label3
             // 
@@ -169,18 +172,19 @@ namespace Part1 {
             label3.BackColor = Color.Transparent;
             label3.Font = new Font("Segoe UI", 14F);
             label3.ForeColor = Color.Cyan;
-            label3.Location = new Point(12, 216);
+            label3.Location = new Point(10, 192);
             label3.Name = "label3";
-            label3.Size = new Size(142, 29);
+            label3.Size = new Size(129, 27);
             label3.TabIndex = 8;
             label3.Text = "Input Code: ";
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(211, 171);
+            textBox2.Enabled = false;
+            textBox2.Location = new Point(185, 152);
             textBox2.Name = "textBox2";
             textBox2.ReadOnly = true;
-            textBox2.Size = new Size(410, 25);
+            textBox2.Size = new Size(359, 23);
             textBox2.TabIndex = 7;
             textBox2.TextChanged += textBox2_TextChanged;
             // 
@@ -190,9 +194,9 @@ namespace Part1 {
             label2.BackColor = Color.Transparent;
             label2.Font = new Font("Segoe UI", 14F);
             label2.ForeColor = Color.Cyan;
-            label2.Location = new Point(12, 165);
+            label2.Location = new Point(10, 147);
             label2.Name = "label2";
-            label2.Size = new Size(180, 29);
+            label2.Size = new Size(163, 27);
             label2.TabIndex = 6;
             label2.Text = "Encoded string: ";
             // 
@@ -202,71 +206,26 @@ namespace Part1 {
             label1.BackColor = Color.Transparent;
             label1.Font = new Font("Segoe UI", 14F);
             label1.ForeColor = Color.Cyan;
-            label1.Location = new Point(12, 9);
+            label1.Location = new Point(10, 8);
             label1.Name = "label1";
-            label1.Size = new Size(406, 29);
+            label1.Size = new Size(369, 27);
             label1.TabIndex = 1;
             label1.Text = "Your string here (40 characters max) ";
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(12, 41);
+            textBox1.Location = new Point(10, 36);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(410, 25);
+            textBox1.Size = new Size(359, 23);
             textBox1.TabIndex = 0;
             textBox1.TextChanged += textBox1_TextChanged;
-            // 
-            // button5
-            // 
-            button5.Location = new Point(745, -5);
-            button5.Name = "button5";
-            button5.Size = new Size(10, 598);
-            button5.TabIndex = 19;
-            button5.Text = "button5";
-            button5.UseVisualStyleBackColor = true;
-            // 
-            // label7
-            // 
-            label7.AutoSize = true;
-            label7.BackColor = Color.Transparent;
-            label7.Location = new Point(11, 77);
-            label7.Name = "label7";
-            label7.Size = new Size(0, 18);
-            label7.TabIndex = 23;
-            // 
-            // label8
-            // 
-            label8.AutoSize = true;
-            label8.BackColor = Color.Transparent;
-            label8.Location = new Point(460, 77);
-            label8.Name = "label8";
-            label8.Size = new Size(0, 18);
-            label8.TabIndex = 24;
-            // 
-            // label9
-            // 
-            label9.AutoSize = true;
-            label9.BackColor = Color.Transparent;
-            label9.Location = new Point(820, 20);
-            label9.Name = "label9";
-            label9.Size = new Size(45, 18);
-            label9.TabIndex = 26;
-            label9.Text = "label9";
-            // 
-            // label11
-            // 
-            label11.AutoSize = true;
-            label11.Location = new Point(191, 20);
-            label11.Name = "label11";
-            label11.Size = new Size(0, 18);
-            label11.TabIndex = 27;
             // 
             // pictureBox1
             // 
             pictureBox1.BackColor = Color.White;
-            pictureBox1.Location = new Point(11, 77);
+            pictureBox1.Location = new Point(10, 68);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(531, 80);
+            pictureBox1.Size = new Size(508, 71);
             pictureBox1.TabIndex = 28;
             pictureBox1.TabStop = false;
             // 
@@ -275,9 +234,9 @@ namespace Part1 {
             label10.AutoSize = true;
             label10.BackColor = Color.White;
             label10.ForeColor = Color.Red;
-            label10.Location = new Point(12, 80);
+            label10.Location = new Point(10, 71);
             label10.Name = "label10";
-            label10.Size = new Size(328, 18);
+            label10.Size = new Size(296, 16);
             label10.TabIndex = 29;
             label10.Text = "❌ String length must be within 1-40 characters!";
             label10.Click += label10_Click;
@@ -287,9 +246,9 @@ namespace Part1 {
             label12.AutoSize = true;
             label12.BackColor = Color.White;
             label12.ForeColor = Color.Red;
-            label12.Location = new Point(12, 98);
+            label12.Location = new Point(10, 87);
             label12.Name = "label12";
-            label12.Size = new Size(302, 18);
+            label12.Size = new Size(281, 16);
             label12.TabIndex = 30;
             label12.Text = "❌ Only uppercase characters are acceptable!";
             label12.Click += label12_Click;
@@ -299,43 +258,61 @@ namespace Part1 {
             label13.AutoSize = true;
             label13.BackColor = Color.White;
             label13.ForeColor = Color.Blue;
-            label13.Location = new Point(12, 118);
+            label13.Location = new Point(10, 105);
             label13.Name = "label13";
-            label13.Size = new Size(445, 18);
+            label13.Size = new Size(176, 16);
             label13.TabIndex = 31;
             label13.Text = "ℹ️ N is in interval [-25, 25]";
             // 
             // numericUpDown1
             // 
-            numericUpDown1.Location = new Point(460, 41);
+            numericUpDown1.Location = new Point(402, 36);
+            numericUpDown1.Maximum = new decimal(new int[] { 25, 0, 0, 0 });
+            numericUpDown1.Minimum = new decimal(new int[] { 25, 0, 0, int.MinValue });
             numericUpDown1.Name = "numericUpDown1";
-            numericUpDown1.Size = new Size(132, 25);
+            numericUpDown1.Size = new Size(116, 23);
             numericUpDown1.TabIndex = 32;
-            numericUpDown1.Minimum = -25;
-            numericUpDown1.Maximum = 25;
-            numericUpDown1.Increment = 1;
-            numericUpDown1.DecimalPlaces = 0;
             numericUpDown1.ValueChanged += numericUpDown1_ValueChanged;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = Color.Transparent;
+            label7.Font = new Font("Segoe UI", 14F);
+            label7.ForeColor = Color.Cyan;
+            label7.Location = new Point(10, 298);
+            label7.Name = "label7";
+            label7.Size = new Size(143, 27);
+            label7.TabIndex = 33;
+            label7.Text = "Input Sort: ";
+            label7.Click += label7_Click;
+            // 
+            // textBox5
+            // 
+            textBox5.Enabled = false;
+            textBox5.Location = new Point(185, 302);
+            textBox5.Name = "textBox5";
+            textBox5.ReadOnly = true;
+            textBox5.Size = new Size(359, 23);
+            textBox5.TabIndex = 34;
+            textBox5.TextChanged += textBox5_TextChanged;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 18F);
+            AutoScaleDimensions = new SizeF(7F, 16F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.LightSteelBlue;
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
             BackgroundImageLayout = ImageLayout.Stretch;
-            ClientSize = new Size(745, 549);
+            ClientSize = new Size(652, 488);
+            Controls.Add(textBox5);
+            Controls.Add(label7);
             Controls.Add(numericUpDown1);
             Controls.Add(label13);
             Controls.Add(label12);
             Controls.Add(label10);
-            Controls.Add(label11);
-            Controls.Add(label9);
-            Controls.Add(label8);
-            Controls.Add(label7);
             Controls.Add(button6);
             Controls.Add(label6);
-            Controls.Add(button5);
             Controls.Add(label5);
             Controls.Add(button4);
             Controls.Add(button3);
@@ -377,17 +354,14 @@ namespace Part1 {
         private Label label2;
         private Label label1;
         private TextBox textBox1;
-        private Button button5;
-        private Label label7;
-        private Label label8;
-        private Label label9;
-        private Label label11;
         private ToolTip toolTip1;
         private PictureBox pictureBox1;
         private Label label10;
         private Label label12;
         private Label label13;
         private NumericUpDown numericUpDown1;
+        private Label label7;
+        private TextBox textBox5;
     }
 }
                                       
