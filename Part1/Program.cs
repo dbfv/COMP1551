6050e345b3e519da3d109c65e91e5b6381885714
dbfv <quanbfclan@gmail.com>
2025-03-29 21:24:52 +0700

    re-format

--- Part1/Program.cs
+++ Part1/Program.cs
@@ -1,7 +1,10 @@
-namespace Part1 {
-    internal static class Program {
+namespace Part1
+{
+    internal static class Program
+    {
         [STAThread]
-        static void Main() {
+        static void Main()
+        {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
